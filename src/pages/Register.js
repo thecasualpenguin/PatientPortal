540c2cda0a90ed20d27f32d500f7fd0514f6987f
casual penguin <20305244+ganc56141@users.noreply.github.com>
2022-07-27 02:10:12 -0400

    basic form inserted for checkout + register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export const Register = () => {
+	return (
+		<React.Fragment>
+			<Form>
+				<Form.Group controlId="formBasicEmail">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control type="email" placeholder="Enter email" />
+					<Form.Text className="text-muted">
+						We'll never share your email with anyone else.
+					</Form.Text>
+				</Form.Group>
 
-  return (
-    <React.Fragment>
-      <h1>Register</h1>
-    </React.Fragment>
-  );
-}
+				<Form.Group controlId="formBasicPassword">
+					<Form.Label>Password</Form.Label>
+					<Form.Control type="password" placeholder="Password" />
+				</Form.Group>
+				<Form.Group controlId="formBasicCheckbox">
+					<Form.Check type="checkbox" label="Check me out" />
+				</Form.Group>
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+		</React.Fragment>
+	);
+};
--- src/pages/ScreeningForm.js
+++ src/pages/ScreeningForm.js
@@ -1,9 +1,36 @@
-import React, {Form} from "react";
-import FormGroup from "react-bootstrap/FormGroup";
-import Container from "react-bootstrap/Container";
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const FormFormat = () => (
+  <Form>
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+    <Form.Group controlId="formBasicCheckbox">
+      <Form.Check type="checkbox" label="Check me out" />
+    </Form.Group>
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+  </Form>
+);
 
 export const ScreeningForm = () => {
 	return (
-    <div></div>
+    <React.Fragment>
+      <div style={{width: '40%', display:'block', marginLeft: 'auto', marginRight: 'auto', marginTop:'5rem'}}>
+        <FormFormat />
+      </div>
+    </React.Fragment>
 	);
 };
